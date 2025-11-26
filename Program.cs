a5afa5bc3c8a7e94b387917c60a8e8905765ebc3
ktrFern <163234159+ktrFern@users.noreply.github.com>
2025-11-27 01:06:50 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Mockups/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Mockups}/{action=Index}/{id?}");
+
+app.Run();
